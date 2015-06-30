b2ac90d48e0c5088e46eb41115a31328e6b5ab96
Jeremy CS <j.crowlesmith@gmail.com>
2015-06-30 16:36:20 +0200

    Basic landing page done

--- web/app/themes/unusualbusiness/templates/head.php
+++ web/app/themes/unusualbusiness/templates/head.php
@@ -2,5 +2,22 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript">
+        WebFontConfig = {
+            google: { families: [ 'Open+Sans:400italic,400,700,300:latin' ] }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })(); </script>
+    <script>
+        // Picture element HTML5 shiv
+        document.createElement( "picture" );
+    </script>
   <?php wp_head(); ?>
 </head>
--- web/app/themes/unusualbusiness/templates/header.php
+++ web/app/themes/unusualbusiness/templates/header.php
@@ -1,12 +1,21 @@
 <header class="banner" role="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+    <div class="container">
+        <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+            <picture>
+                <img srcset="<?= get_bloginfo('template_directory');?>/dist/images/unusualbusiness-logo.png, <?= get_bloginfo('template_directory');?>/dist/images/unusualbusiness-logo-retina.png 2x"
+                     alt="Linksmith - Web development" class="logo" />
+            </picture>
+        </a>
+        <?php
+        /*
+          <nav role="navigation">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+            endif;
+            ?>
+          </nav>
+          */
+        ?>
+    </div>
 </header>
