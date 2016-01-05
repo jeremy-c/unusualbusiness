9785932e49f28b1e11db6632c5d46a916093c6fc
Vacant City <vacantcity@users.noreply.github.com>
2016-01-05 16:31:07 +0100

    OrganizationPage fix

--- organizations/models.py
+++ organizations/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext as _
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, TabbedInterface, ObjectList, \
     StreamFieldPanel
 from wagtail.wagtailcore.fields import RichTextField, StreamField
@@ -18,17 +19,46 @@ class OrganizationPage(TranslationMixin, Page):
 
     # Database fields
 
-    description = RichTextField()
-    date_founded = models.DateField("Founded date")
-    feed_image = models.ForeignKey(
+    description = RichTextField(
+        verbose_name = _("Description"),
+        null=True
+    )
+    date_founded = models.DateField(
+        verbose_name = _("Founded date"),
+        null=True
+    )
+    amount_of_members = models.PositiveIntegerField(
+        verbose_name = _("Amount of members"),
+        null=True
+    )
+    # This should probably be a specific geolocation field:
+    location = models.CharField(
+        verbose_name = _("Location"),
+        max_length=512,
+        blank=True
+    )
+    email = models.EmailField(
+        verbose_name = _("Email"),
+        blank=True
+    )
+    website = models.URLField(
+        verbose_name = _("Website"),
+        blank=True
+    )
+    facebook = models.URLField(
+        verbose_name = _("Facebook"),
+        blank=True
+    )
+    featured_image = models.ForeignKey(
         'wagtailimages.Image',
+        verbose_name = _("Featured image"),
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name='+'
     )
 
-    # Search index configuraiton
+    # Search index configuration
 
     search_fields = Page.search_fields + (
         index.SearchField('title_en'),
@@ -38,13 +68,17 @@ class OrganizationPage(TranslationMixin, Page):
         index.FilterField('date_founded'),
     )
 
-
     # Editor panels configuration
 
     content_panels = Page.content_panels + [
         FieldPanel('date_founded'),
         FieldPanel('description_en', classname="full"),
         FieldPanel('description_nl', classname="full"),
+        FieldPanel('location'),
+        FieldPanel('email'),
+        FieldPanel('website'),
+        FieldPanel('facebook'),
+        ImageChooserPanel('featured_image')
     ]
     #
     # dutch_content_panels = [
