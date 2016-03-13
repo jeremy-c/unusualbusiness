4d9a45070dc2cd9fd37890a3d8bd07249c8583a7
Jeremy CS <j.crowlesmith@gmail.com>
2016-03-13 13:13:43 +0100

    Agenda hover & Javascript event modules

--- unusualbusiness/assets/scripts/main.js
+++ unusualbusiness/assets/scripts/main.js
@@ -7,11 +7,45 @@
 
 console.log('Hello, World!');
 
-(function() {
-  $(document).ready(function() {
+var featuredAgenda = () => {
+  let initMouseEvents = function() {
+    let agendaItemLink = $('.featured-agenda-item-link');
+
+    agendaItemLink.on('mouseover', function() {
+      let timeElement = $(this).parent().siblings().first();
+      timeElement.addClass('is-hovered');
+    });
+    agendaItemLink.on('mouseout', function() {
+      let timeElement = $(this).parent().siblings().first();
+      timeElement.removeClass('is-hovered');
+    });
+  };
+
+  return {
+    initMouseEvents: initMouseEvents
+  };
+};
+
+var featuredArticles = () => {
+  let initSlider = function() {
     $('.slider').unslider({
       keys: false,
       arrows: false,
-      nav: true});
+      nav: true
+    });
+  };
+
+  return {
+    initSlider: initSlider
+  };
+};
+
+(function() {
+  $(document).ready(function() {
+    let FeaturedArticles = featuredArticles();
+    FeaturedArticles.initSlider();
+
+    let FeaturedAgenda = featuredAgenda();
+    FeaturedAgenda.initMouseEvents();
   });
 })();
