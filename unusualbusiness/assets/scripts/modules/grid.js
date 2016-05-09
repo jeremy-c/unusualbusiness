a276021ba5a12556780c7e9f73b26a9178893f4c
Jeremy CS <j.crowlesmith@gmail.com>
2016-05-09 13:50:02 +0200

    Home: hover states

--- unusualbusiness/assets/scripts/modules/grid.js
+++ unusualbusiness/assets/scripts/modules/grid.js
@@ -22,9 +22,21 @@ let Grid = () => {
     });
   };
 
+  let toggleHoverClass = function() {
+    let overlayElement = $(this).parent().toggleClass('is-hovered');
+  };
+
+  let initHoverStates = function() {
+    let gridLink = $('.l-grid-item a');
+
+    gridLink.on('mouseover', toggleHoverClass);
+    gridLink.on('mouseout', toggleHoverClass);
+  };
+
   function init() {
     console.log('Grid go!');
     initIsotope();
+    initHoverStates();
   }
 
   return {
