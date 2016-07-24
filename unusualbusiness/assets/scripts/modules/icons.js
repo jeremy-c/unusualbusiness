677e960c843534ba83b4d53e89a7de27639b8a33
Jeremy CS <j.crowlesmith@gmail.com>
2016-07-24 11:54:22 +0200

    Icon animation hack

--- unusualbusiness/assets/scripts/modules/icons.js
+++ unusualbusiness/assets/scripts/modules/icons.js
@@ -3,6 +3,8 @@
    ======================= */
 
 'use strict';
+import debounce from 'debounce';
+
 
 let Icons = () => {
 
@@ -23,30 +25,87 @@ let Icons = () => {
         // event.currentTarget.classList.remove("is-running");
     };
 
-    let startAnimation = function(){
-        let element = this;
-        let svgElements = element.querySelectorAll('svg *');
+    let whichAnimationEvent = function() {
+      var t,
+          el = document.createElement("fakeelement");
 
-        for (var j = 0; j < svgElements.length; j++) {
-            let svgElement = svgElements[j];
-            svgElement.style.webkitAnimationPlayState = "running";
+      var animations = {
+        "animation"      : "animationend",
+        "OAnimation"     : "oAnimationEnd",
+        "MozAnimation"   : "animationend",
+        "WebkitAnimation": "webkitAnimationEnd"
+      };
+
+      for (t in animations){
+        if (el.style[t] !== undefined){
+          return animations[t];
         }
-        // element.classList.add("is-running");
-
-        element.addEventListener('webkitAnimationEnd', stopAnimation);
-        element.addEventListener('mozAnimationEnd', stopAnimation);
-        element.addEventListener('oAnimationEnd', stopAnimation);
-        element.addEventListener('msAnimationEnd', stopAnimation);
-        element.addEventListener('animationEnd', stopAnimation);
+      }
     };
 
     let initStartSVGAnimationsOnHover = function() {
-        let animationHolders = document.getElementsByClassName("start-animation-on-hover");
 
-        for (var i = 0; i < animationHolders.length; i++) {
-            let element = animationHolders[i];
-            element.addEventListener("mouseenter", startAnimation, false);
+        $(".start-animation-on-hover").on('mouseover', debounce(iterateAnimation));
+    };
+
+    let iterateAnimation = function() {
+        let iteration = 1;
+        let $this = $(this);
+        let animationEvent = whichAnimationEvent();
+
+        $this.addClass("is-running");
+
+        if ( $this.attr('data-iteration') ) {
+            iteration = parseInt( $this.attr('data-iteration') );
+            $this.attr('data-iteration', (iteration + 1).toString());
+        } else {
+            $this.attr('data-iteration', (iteration).toString());
         }
+
+        switch ( iteration ) {
+            case 1:
+                $this.removeClass('is-iteration-1');
+                $this.addClass('is-iteration-2');
+                break;
+            case 2 :
+                $this.removeClass('is-iteration-2');
+                $this.addClass('is-iteration-3');
+                break;
+            case 3 :
+                $this.removeClass('is-iteration-3');
+                $this.addClass('is-iteration-4');
+                break;
+            case 4 :
+                $this.removeClass('is-iteration-4');
+                $this.addClass('is-iteration-5');
+                break;
+            case 5 :
+                $this.removeClass('is-iteration-5');
+                $this.addClass('is-iteration-6');
+                break;
+            case 6 :
+                $this.removeClass('is-iteration-6');
+                $this.addClass('is-iteration-7');
+                break;
+            case 7 :
+                $this.removeClass('is-iteration-7');
+                $this.addClass('is-iteration-8');
+                break;
+            case 8 :
+                $this.removeClass('is-iteration-8');
+                $this.addClass('is-iteration-9');
+                break;
+            case 9 :
+                $this.removeClass('is-iteration-9');
+                break;
+        }
+
+        $(this).one(animationEvent, endAnimation);
+    };
+
+    let endAnimation = function(event) {
+        console.log('ended');
+        $(this).removeClass("is-running");
     };
 
   let init = function() {
